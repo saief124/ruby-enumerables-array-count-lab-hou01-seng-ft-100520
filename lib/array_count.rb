6f7b0b75121e0c27c519fde3373ec0fcfa667b04
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-19 07:37:59 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,6 +1,6 @@
 def count_strings(array)
 array.count do |string|
-  string>0
+  string==""
   end
 
   # Return the total number of strings in the provided array using the count enumerable
@@ -8,7 +8,7 @@ end
 
 def count_empty_strings(array)
 array.count do|string|
-string>0
+string==""
 end
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
