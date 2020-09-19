9d0fc6bf4eb2224002b28625627135ecd63cf922
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-19 07:45:43 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,6 +1,6 @@
 def count_strings(array)
 array.count do |string|
-  string.instance_of?('')
+  string.instance_of?(String)
   end
 
   # Return the total number of strings in the provided array using the count enumerable
