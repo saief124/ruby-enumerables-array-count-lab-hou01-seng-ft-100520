51735fca4bed2ade6d2d7bbecf5149906e65c9f9
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-19 07:39:02 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,6 +1,6 @@
 def count_strings(array)
 array.count do |string|
-  string==""
+  string.include?""
   end
 
   # Return the total number of strings in the provided array using the count enumerable
