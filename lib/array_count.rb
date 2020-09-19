a927ec7e499aeae032a550c2bc7eff74823b8028
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-19 07:25:30 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -6,5 +6,9 @@ end
 end
 
 def count_empty_strings(array)
+array.count do|string|
+  if string.include?"" && string.length==0
+    string
+  end
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
